b11811548441c79681ff6453bb703224ff4f92b5
Chifeng Wen <daviesx66@gmail.com>
2023-05-06 07:09:32 -0700

    Adds a test for the health checker component.

---
+++ account/src/test/java/org/e8/healthchecker/component/TestHealthCheckerComponent.java
@@ -0,0 +1,28 @@
+package org.e8.healthchecker.component;
+
+import org.e8.healthchecker.component.EmptyHealthCheckerException;
+import org.e8.healthchecker.component.HealthCheckerComponent;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+public class TestHealthCheckerComponent {
+    @Autowired
+    private HealthCheckerComponent comp;
+    
+    @Test
+    public void testQueryLatestId() throws EmptyHealthCheckerException {
+        String latestId = comp.queryLatestId();
+        Assert.assertEquals("e574abc3-2a08-4cd5-8324-34c1a5500c23", latestId);
+        
+        String latestId2 = comp.queryLatestId();
+        Assert.assertEquals(latestId, latestId2);
+    }
+}
